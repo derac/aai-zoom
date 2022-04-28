e2d92a49cc763e0567bbcfb4b51d09b713c11616
derac <dwolson123@gmail.com>
2022-04-28 14:36:01 -0500

    set up basic socketio

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app)
+
+
+@socketio.on("connect")
+def connect():
+    emit("message", {"hello": "Hello"})
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, port=80)
